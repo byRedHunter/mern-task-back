b70af08013deab4c51902891c53fc961404913d9
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-04-21 16:04:18 -0500

    crear un usurio, simple

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,19 @@
-exports.createUsuario = (req, res) => {
-	console.log(req.body)
-	res.send('creamos un usuario')
+const Usuario = require('../models/Usuario')
+
+exports.createUsuario = async (req, res) => {
+	try {
+		let usuario
+
+		// crea el nuevo usuario
+		usuario = new Usuario(req.body)
+
+		// guarda el usuario
+		await usuario.save()
+
+		// mensaje de confirmacion
+		res.send('Usuario creado correctamente')
+	} catch (error) {
+		console.log(error)
+		res.status(400).send('Hubo un error.')
+	}
 }
