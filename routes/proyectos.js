f944d0b3147268e57892ef4951e7e2ba8200c706
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-04-22 10:50:16 -0500

    middleware para ver si el usurio esta autenticado y guard el proyecto con el id del usuario

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const { crearProyecto } = require('../controllers/proyectoController')
+const auth = require('../middleware/auth')
 const router = express.Router()
 
 // crear proectos
 // api/proyectos
-router.post('/', crearProyecto)
+router.post('/', auth, crearProyecto)
+router.get('/', auth, crearProyecto)
 
 module.exports = router
