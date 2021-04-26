3d12328847ef8b558a6000794c32523fb672a7f7
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-04-26 17:40:18 -0500

    obtener dato del usuario autenticado

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const { check } = require('express-validator')
 const { autenticarUsuario } = require('../controllers/authController')
+const auth = require('../middleware/auth')
 const router = express.Router()
 
 // api/auth
+
+// iniciar sesion
 router.post('/', [
 	check('email', 'Agrega un email valido.').isEmail(),
 	check('password', 'El password deb ser minimo de 6 caracteres').isLength({
@@ -11,5 +14,7 @@ router.post('/', [
 	}),
 	autenticarUsuario,
 ])
+
+router.get('/', auth, usuarioAutenticado)
 
 module.exports = router
